164f6412edb711cdf3d29bd611f75609c1aee9c4
Laurent Demailly <ldemailly@gmail.com>
2025-06-06 16:09:10 -0700

    document better and remove unused unistd include

--- misc-c/happynull.c
+++ misc-c/happynull.c
@@ -1,15 +1,16 @@
+// Works on Linux with
+// echo 0 > /proc/sys/vm/mmap_min_addr
 #include <sys/mman.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <unistd.h>
 
-
-// Works on Linux with
-// echo 0 > /proc/sys/vm/mmap_min_addr
-// init function that runs before main()
+// The trick part:
+// init function that runs before main(), just to be
+// able to share main()'s full body for "quizz" purposes,
+// otherwise we'd just call it explicitly from main().
 __attribute__((constructor))
-void pagingDrZero(void) {
+static void pagingDrZero(void) {
   void *start = (void *)0;
   void *p = mmap(start, 4096, PROT_READ | PROT_WRITE, MAP_FIXED | MAP_ANON | MAP_PRIVATE, -1, 0);
   if (errno != 0) {
@@ -21,6 +22,7 @@ void pagingDrZero(void) {
   }
 }
 
+// The fun part:
 int main(void) {
   int *p = NULL;
   printf("we're going to read and write to pointer: %p (and not crash)\n", (void*)p);
