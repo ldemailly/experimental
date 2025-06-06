0370fdb67062b4a0faaef8aca5e8b07de5010dd6
Laurent Demailly <ldemailly@gmail.com>
2025-06-06 15:54:18 -0700

    Adding pre main function

--- misc-c/happynull.c
+++ misc-c/happynull.c
@@ -7,6 +7,8 @@
 
 // Works on Linux with
 // echo 0 > /proc/sys/vm/mmap_min_addr
+// init function that runs before main()
+__attribute__((constructor))
 void pagingDrZero(void) {
   void *start = (void *)0;
   void *p = mmap(start, 4096, PROT_READ | PROT_WRITE, MAP_FIXED | MAP_ANON | MAP_PRIVATE, -1, 0);
@@ -14,15 +16,17 @@ void pagingDrZero(void) {
       perror("failed alloc did you echo 0 > /proc/sys/vm/mmap_min_addr ?");
       exit(1);
   }
-  printf("Allocated page at %p\n", p);
+  if (p) {
+    printf("Allocated page at %p\n", p);
+  }
 }
 
 int main(void) {
-  pagingDrZero();
   int *p = NULL;
+  printf("we're going to read and write to pointer: %p (and not crash)\n", (void*)p);
   int x;
   x = *p; // read from NULL pointer
-  printf("%p %d\n", (void*)p, x);
+  printf("%d\n", x);
   *p = 42; // write to NULL pointer
   printf("%d\n", *p);
   return 0;
