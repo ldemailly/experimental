97b26e3a433aec4047499539eb29d50be8856b94
Laurent Demailly <ldemailly@gmail.com>
2026-03-22 17:29:30 -0700

    add hotreload-demo target and better naming

--- misc-c/hotreload.c
+++ misc-c/hotreload.c
@@ -11,7 +11,7 @@ int myFunc1(int inp) {
 }
 
 // read (could be from network, using a file to keep this simple)
-char *read_data(char *fname) {
+char *load_code(char *fname) {
     FILE *f = fopen(fname, "rb");
     if (!f) {
         perror("fopen");
@@ -20,13 +20,11 @@ char *read_data(char *fname) {
     fseek(f, 0, SEEK_END);
     long fsize = ftell(f);
     fseek(f, 0, SEEK_SET);
-
     if (fsize <= 0) {
         fclose(f);
         fprintf(stderr, "Invalid file size\n");
         return NULL;
     }
-
     char *data = mmap(NULL, (size_t)fsize, PROT_READ | PROT_WRITE,
                       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     if (data == MAP_FAILED) {
@@ -34,22 +32,18 @@ char *read_data(char *fname) {
         perror("mmap");
         return NULL;
     }
-
     size_t nread = fread(data, 1, (size_t)fsize, f);
     fclose(f);
-
     if (nread != (size_t)fsize) {
         munmap(data, (size_t)fsize);
         fprintf(stderr, "Short read\n");
         return NULL;
     }
-
     if (mprotect(data, (size_t)fsize, PROT_READ | PROT_EXEC) != 0) {
         munmap(data, (size_t)fsize);
         perror("mprotect");
         return NULL;
     }
-
     return data;
 }
 
@@ -59,9 +53,9 @@ int main(void) {
     printf("input: ");
     scanf("%d", &inp);
     printf("myFunc1(%d) = %d\n", inp, myFunc(inp));
-    char *data = read_data("data.bin");
+    char *data = load_code("code.bin");
     if (!data) {
-        fprintf(stderr, "Failed to read data\n");
+        fprintf(stderr, "Failed to read new code\n");
         return 1;
     }
     // assign the pointer (-Wpedantic doesn't like the simpler/direct cast)
