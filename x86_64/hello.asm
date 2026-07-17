6e7d4cf58719f6bb6158c25f28910bab8d03a854
Laurent Demailly <ldemailly@gmail.com>
2026-07-16 18:42:48 -0700

    Simplified the elf vs macho thanks to %pragma, also can have \n inside ` backticks

--- x86_64/hello.asm
+++ x86_64/hello.asm
@@ -1,28 +1,23 @@
 %ifidn __OUTPUT_FORMAT__, macho64
     build_version macos, 14, 0
-    extern _printf
-    %define print_fn _printf
-    global _main
-    %define main_lbl _main
     %define extra_msg "Mach-O 64"
 %else
-    extern printf
-    %define print_fn printf
-    global main
-    %define main_lbl main
     %define extra_msg "ELF64"
 %endif
 
-LF equ 10
+%pragma macho64 gprefix _
+
+extern printf
+global main
 
 section .text
-main_lbl: 
+main:
     sub     rsp, 8              ; Align stack to 16 bytes before calling function
     lea     rdi, [rel msg]      ; Load address of the message (RIP-relative)
-    call    print_fn             ; Call standard C library function
+    call    printf              ; Call standard C library function
     add     rsp, 8              ; Restore stack pointer
     mov     rax, 0              ; Return 0
     ret
 
 section .data
-msg:    db "Hello, x86_64 World from Apple Silicon! (", extra_msg, ")", LF, 0
+msg:    db `Hello, x86_64 World from Apple Silicon! (`, extra_msg, `)\n`, 0
